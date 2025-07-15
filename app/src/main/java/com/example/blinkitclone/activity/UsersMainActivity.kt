abd4541be7cd4c787e2b5e666413f8a8d853cf3c
Vanika <vanika@REVS100300.local>
2025-07-15 15:33:30 +0530

    add-functionality-of-splash-signIn-and-otp

--- app/src/main/java/com/example/blinkitclone/activity/UsersMainActivity.kt
+++ app/src/main/java/com/example/blinkitclone/activity/UsersMainActivity.kt
@@ -1,9 +1,10 @@
-package com.example.blinkitclone
+package com.example.blinkitclone.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.blinkitclone.R
 
-class MainActivity : AppCompatActivity() {
+class UsersMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ app/src/main/java/com/example/blinkitclone/models/Users.kt
@@ -0,0 +1,7 @@
+package com.example.blinkitclone.models
+
+data class Users(
+    var uid : String? = null,
+    var userPhoneNumber: String? = null,
+    val userAddress : String? = null
+)
